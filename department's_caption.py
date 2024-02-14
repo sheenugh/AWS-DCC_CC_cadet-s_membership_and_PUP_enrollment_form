9e388df2ea209fc5428e289a5e3acfdff074d2eb
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 14:41:56 +0800

    pseudo code

--- department's_caption.py
+++
@@ -1,7 +0,0 @@
-ask_user_name = input("Input Your Full Name:")
-ask_user_the_department = input("Enter your AWS Cloud Club Department:")
-ask_user_the_galaxy_belong = input("Specify the galaxy to which you belong:")
-
-print("\n")
-Sheen
-print("🚀Deploying...I'm <𝗡𝗮𝗺𝗲>, representing the <𝗗𝗲𝗽𝗮𝗿𝘁𝗺𝗲𝗻𝘁>. I'm thrilled to commence our expedition into the vast and uncharted realm of the <𝗚𝗮𝗹𝗮𝘅𝘆>. Together, we'll navigate this exciting frontier of technology! 🌌🌟💻 ")
---
+++ pup_enrollment_form.py
@@ -0,0 +1,5 @@
+# ========== PSEUDO CODE ========
+# - Printing the 'PUP ENROLLMENT FORM'
+# - Ask user's information
+# - Displaying the user's inputted information
+
