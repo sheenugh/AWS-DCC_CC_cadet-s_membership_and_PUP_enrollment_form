2d3459cacce3ab144e6b452c7da486b7a488acd9
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 14:44:41 +0800

    pseudo code

---
+++ pup_enrollment_forrm.py
@@ -0,0 +1,12 @@
+# ========== PSEUDO CODE ========
+# - Printing the 'PUP ENROLLMENT FORM'
+# - Ask user's information
+# - Displaying the user's inputted information
+
+
+# ========== PSEUDO CODE ========
+# - Printing the 'PUP ENROLLMENT FORM'
+# - Ask user's information
+# - Displaying the user's inputted information
+
+
