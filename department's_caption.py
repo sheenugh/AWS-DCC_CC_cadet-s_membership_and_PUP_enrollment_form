4e9f083d541a132ef4e9635ee9a1b6afa3b3da6b
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 14:07:12 +0800

    asking the user's name, what dept, what galaxy he/she is belong

---
+++ department's_caption.py
@@ -0,0 +1,4 @@
+ask_user_name = input("Input Your Name:")
+ask_user_the_department = input("Enter your AWS Cloud Club Department:")
+ask_user_the_galaxy_belong = input("Specify the galaxy to which you belong:")
+
