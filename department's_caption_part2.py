49ccb8ab01dab9256ae84fbb704dbc45fc682b9f
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 14:28:52 +0800

    done

--- department's_caption_part2.py
+++ department's_caption_part2.py
@@ -1,3 +1,12 @@
 # ========== PSEUDO CODE ========
 # - Ask user's information 
+ask_user_name = input("Input Your Full Name:")
+ask_user_the_department = input("Enter your AWS Cloud Club Department:")
+ask_user_the_galaxy_belong = input("Specify the galaxy to which you belong:")
+
+print("\n")
+
 # - Printing the DCC DP Blast
+print("Thank You for providing your information.")
+print("Now, here is your Department's DP Blast Caption:")
+print("🚀Deploying...I'm <𝗡𝗮𝗺𝗲>, representing the <𝗗𝗲𝗽𝗮𝗿𝘁𝗺𝗲𝗻𝘁>. I'm thrilled to commence our expedition into the vast and uncharted realm of the <𝗚𝗮𝗹𝗮𝘅𝘆>. Together, we'll navigate this exciting frontier of technology! 🌌🌟💻 ")
